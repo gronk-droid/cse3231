e9f34a83d7b32869cb2fd9220ab18be49a065ea9
gronk-droid <grantbutler7@gmail.com>
2022-03-24 14:59:36 -0400

    socket connect

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -6,6 +6,7 @@ email = 'gbutler2020@my.fit.edu'
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        soc.connect((HOST, PORT))
         soc.sendto(email.encode(), (HOST, PORT))
         print('sent: ', email)
         data = soc.recv(1024)
