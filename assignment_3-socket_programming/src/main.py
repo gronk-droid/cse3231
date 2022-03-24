15b1b74528dc73d82e09c1c4ae3fe7ae07b7e346
gronk-droid <grantbutler7@gmail.com>
2022-03-24 14:57:09 -0400

    reformat

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -5,7 +5,7 @@ email = 'gbutler2020@my.fit.edu'
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.sendto(email.encode(), (HOST,PORT))
+        soc.sendto(email.encode(), (HOST, PORT))
         print('sent: ', email)
         data = soc.recv(1024)
         print('Received: '+data[0].decode())
