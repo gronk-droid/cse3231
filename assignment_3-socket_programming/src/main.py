ae193ce3155aee192d49ebb9d1b9fa134faefcda
Grant Butler <grantbutler7@gmail.com>
2022-03-24 14:53:41 -0400

    fin

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -5,11 +5,10 @@ email = 'gbutler2020@my.fit.edu'
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.connect((HOST, PORT))
-        soc.sendall(email.encode())
+        soc.sendto(email.encode(), (HOST,PORT))
         print('sent: ', email)
         data = soc.recv(1024)
-        print('Received: '+data.decode())
+        print('Received: '+data[0].decode())
 
 
 main()
