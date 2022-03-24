27f02c0bc3c89625cb516e325e95c78c88dd95d8
Grant Butler <grantbutler7@gmail.com>
2022-03-24 15:27:06 -0400

    finished

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -1,13 +1,14 @@
 import socket
 HOST = '34.74.19.25'
 PORT = 30315
+SERVER_ADDR = (HOST, PORT)
 email = 'gbutler2020@my.fit.edu'
 
 
 def main():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.connect((HOST, PORT))
-    soc.sendto(email.encode(), (HOST, PORT))
+    soc.connect(SERVER_ADDR)
+    soc.sendto(email.encode(), SERVER_ADDR)
     print('sent: ', email)
     data = soc.recv(1024)
     print('Received: '+data[0].decode())
