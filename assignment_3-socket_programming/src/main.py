26aafff47fd68e52844d027912cfb50896ceec96
gronk-droid <grantbutler7@gmail.com>
2022-03-24 14:57:25 -0400

    pep-8 format

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -3,6 +3,7 @@ HOST = '34.74.19.25'
 PORT = 30315
 email = 'gbutler2020@my.fit.edu'
 
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
         soc.sendto(email.encode(), (HOST, PORT))
