2314c0e63364ba876f31676f0f75509bed710046
Grant Butler <grantbutler7@gmail.com>
2022-03-24 15:05:08 -0400

    remove with statement

--- assignment_3-socket_programming/src/main.py
+++ assignment_3-socket_programming/src/main.py
@@ -5,12 +5,12 @@ email = 'gbutler2020@my.fit.edu'
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.connect((HOST, PORT))
-        soc.sendto(email.encode(), (HOST, PORT))
-        print('sent: ', email)
-        data = soc.recv(1024)
-        print('Received: '+data[0].decode())
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect((HOST, PORT))
+    soc.sendto(email.encode(), (HOST, PORT))
+    print('sent: ', email)
+    data = soc.recv(1024)
+    print('Received: '+data[0].decode())
 
 
 main()
